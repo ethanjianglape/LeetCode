8f2d6ba55d5a85f69bb76f55d899258b139d4594
Ethan Jiang-Lape <ejiang-lape@abovethetreeline.com>
2024-12-30 16:14:56 -0500

    Adding more solutions

---
+++ Solutions/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * This file is part of the LeetCode project.
+ *  Copyright (C) 2024 Ethan Jiang-Lape
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+namespace LeetCode.Solutions;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        ArgumentNullException.ThrowIfNull(s);
+
+        int i = 0;
+        int max = 0;
+
+        Queue<char> queue = new();
+
+        while (i < s.Length)
+        {
+            char c = s[i];
+
+            if (queue.Contains(c))
+            {
+                queue.Dequeue();
+            }
+            else
+            {
+                queue.Enqueue(c);
+                max = Math.Max(max, queue.Count);
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ Solutions/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,76 @@
+﻿/**
+ * This file is part of the LeetCode project.
+ *  Copyright (C) 2024 Ethan Jiang-Lape
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+namespace LeetCode.Solutions;
+
+public class MedianOfTwoSortedArrays
+{
+    public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        ArgumentNullException.ThrowIfNull(nums1);
+        ArgumentNullException.ThrowIfNull(nums2);
+
+        int i = 0;
+        int j = 0;
+
+        List<double> merged = new(nums1.Length + nums2.Length);
+
+        while (i < nums1.Length && j < nums2.Length)
+        {
+            int num1 = nums1[i];
+            int num2 = nums2[j];
+
+            if (num1 < num2)
+            {
+                merged.Add(num1);
+                i++;
+            }
+            else
+            {
+                merged.Add(num2);
+                j++;
+            }
+        }
+
+        while (i < nums1.Length)
+        {
+            merged.Add(nums1[i++]);
+        }
+
+        while (j < nums2.Length)
+        {
+            merged.Add(nums2[j++]);
+        }
+
+        int length = merged.Count;
+
+        if (length % 2 == 0)
+        {
+            int rightIndex = length / 2;
+            int leftIndex = rightIndex - 1;
+
+            return (merged[leftIndex] + merged[rightIndex]) / 2;
+        }
+
+        int index = length / 2;
+
+        return merged[index];
+    }
+}
