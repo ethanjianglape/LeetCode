91f585b7d000af97eadb5a85d67560b768323c88
Ethan Jiang-Lape <ejiang-lape@abovethetreeline.com>
2024-12-30 10:52:49 -0500

    Add project files.

---
+++ TwoSum.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/two-sum/description/
+
+namespace LeetCode;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        ArgumentNullException.ThrowIfNull(nums);
+        ArgumentOutOfRangeException.ThrowIfLessThan(nums.Length, 2);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num1 = nums[i];
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                int num2 = nums[j];
+
+                if (num1 + num2 == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+}
